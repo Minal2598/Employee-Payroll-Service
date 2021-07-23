c9f78706c1cfdb26edda333fdca938accc97af30
Minal <patilminalp@gmail.com>
2021-07-23 21:56:29 +0530

    Added:Employee-Payroll-Service using MySql

---
+++ employee_Payroll.sql
@@ -0,0 +1,59 @@
+#UC1
+create database payroll_service;
+show databases;
+use payroll_service;
+
+#UC2
+create table employee_payroll(
+	id int unsigned not null auto_increment,
+	name varchar (150) not null,
+	salary double not null,
+    start date not null,
+    primary key (id)
+
+);
+
+
+#UC3
+insert into employee_payroll (name,salary,start) values
+('Minal',10000.0,'2021-01-03'),
+('Minu',30000.0,'2020-02-05'),
+('Mina',5000.0,'2021-07-04'),
+('Kalpesh',7000.0,'2021-04-11');
+
+#UC4
+select * from employee_payroll;
+
+#UC5
+
+select * from employee_payroll where name = 'Minal'; 
+select * from employee_payroll where start between cast('2021-01-01' As date) and Date (now());
+
+#UC6
+Alter table employee_payroll add gender varchar(1) after name;
+
+update employee_payroll set gender = 'F' where name = 'Minal' or name ='Minu' or name ='Mina';
+update employee_payroll set gender = 'M' where name = 'Kalpesh';
+
+
+#UC7
+select salary from employee_payroll;
+select sum(salary) from employee_payroll;
+select min(salary) from employee_payroll;
+select max(salary) from employee_payroll;
+select count(salary) from employee_payroll;
+select avg(salary) from employee_payroll;
+select sum(salary) from employee_payroll where gender = 'F' group by gender;
+
+
+
+
+
+
+
+
+
+
+
+
+
